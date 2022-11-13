fbb337176778182752dea6f8372fbe27fa2c582d
LeonardoConstantino <76814929+LeonardoConstantino@users.noreply.github.com>
2022-11-13 14:46:33 -0300

    Create frases.js

---
+++ frases.js
@@ -0,0 +1,166 @@
+export const frases = [
+    {
+        "autor": "Michael John Bobak",
+        "frase": "Todo processo acontece fora da zona de conforto."
+    },
+    {
+        "autor": "Henrique Carvalho",
+        "frase": "A maioria das dicas que você recebe do seu gerente de banco ou fóruns de investimentos refletem o interesse deles, não os seus."
+    },
+    {
+        "autor": "Douglas Gonçalves",
+        "frase": "Aprender a controlar seu orçamento é o modo mais prático de cortar gastos e começar a investir."
+    },
+    {
+        "autor": "Benjamin Franklin",
+        "frase": "Investir em conhecimento rende sempre os melhores juros."
+    },
+    {
+        "autor": "Michael Batnick",
+        "frase": "Evitar erros catastróficos é mais importante do que construir o portfólio perfeito."
+    },
+    {
+        "autor": "Michael Kitces",
+        "frase": "Invista pensando no longo prazo, não especule, mas, não ignore as flutuações do mercado."
+    },
+    {
+        "autor": "Dale Carnegie",
+        "frase": "Em vez de se preocupar com o que as pessoas dizem sobre você, por que não investir tempo tentando fazer algo que elas admirem?"
+    },
+    {
+        "autor": "Rafael Seabra",
+        "frase": "Depois que você tem uma base sólida de conhecimento, fica muito mais fácil aprender a investir e lidar com dinheiro."
+    },
+    {
+        "autor": "Steve Siebold",
+        "frase": "A maneira mais rápida de ganhar dinheiro é resolver um problema. Quanto maior for o problema a resolver, mais dinheiro que você vai ganhar."
+    },
+    {
+        "autor": "Will Smith",
+        "frase": "Dinheiro e sucesso não mudam as pessoas, eles só ampliam o que já está lá."
+    },
+    {
+        "autor": "Augusto Cury",
+        "frase": "Você precisa conquistar aquilo que o dinheiro não compra. Caso contrário, será um miserável, ainda que seja um milionário."
+    },
+    {
+        "autor": "Ayn Rand",
+        "frase": "Dinheiro é apenas uma ferramenta. Ele irá levá-lo onde quiser, mas não vai substituí-lo como motorista."
+    },
+    {
+        "autor": "Benjamin Franklin",
+        "frase": "Se você almeja ser rico, pense em poupar assim como você pensa em ganhar dinheiro."
+    },
+    {
+        "autor": "Warren Buffet",
+        "frase": "Risco vem de você não saber o que está fazendo. Controle o seu dinheiro."
+    },
+    {
+        "autor": "Thomas Jefferson",
+        "frase": "Jamais gaste seu dinheiro antes de você possuí-lo."
+    },
+    {
+        "autor": "Harry F. Banks",
+        "frase": "Para o sucesso, atitude é igualmente tão importante quanto capacidade."
+    },
+    {
+        "autor": "Robert Collier",
+        "frase": "Sucesso é a soma de pequenos esforços, repetidos o tempo todo."
+    },
+    {
+        "autor": "Confúcio",
+        "frase": "A diferença entre um homem de sucesso e outro orientado para o fracasso é que um está aprendendo a errar, enquanto o outro está procurando aprender com seus próprios erros."
+    },
+    {
+        "autor": "Henry David Thoreau",
+        "frase": "O sucesso vem geralmente àqueles que estão muito ocupados para estar procurando por ele."
+    },
+    {
+        "autor": "William Feather",
+        "frase": "Sucesso parece ser, em grande parte, uma questão de continuar depois que outros desistiram."
+    },
+    {
+        "autor": "Virgílio",
+        "frase": "O sucesso encoraja-os: eles podem porque pensam que podem."
+    },
+    {
+        "autor": "Frank Lloyrd Wright",
+        "frase": "Eu sei o preço do sucesso: dedicação, trabalho duro, e uma incessante devoção às coisas que você quer ver acontecer."
+    },
+    {
+        "autor": "Thomas J Stanley",
+        "frase": "Antes de se tornar um milionário, você deve aprender a pensar como tal. Aprenda a se motivar para combater o medo com coragem."
+    },
+    {
+        "autor": "Renan Bride de Oliveira",
+        "frase": "Planejamento, organização e motivação são fatores essenciais para a prosperidade."
+    },
+    {
+        "autor": "Jim Ryun",
+        "frase": "Motivação é aquilo que te faz começar. Hábito é aquilo que te faz continuar."
+    },
+    {
+        "autor": "John Jacob Astor",
+        "frase": "A riqueza é o resultado dos seus hábitos diários."
+    },
+    {
+        "autor": "Aristóteles",
+        "frase": "Nós somos aquilo que fazemos repetidamente. Excelência, então, não é um modo de agir, mas um hábito."
+    },
+    {
+        "autor": "Aristóteles",
+        "frase": "O que sabemos aprendemos fazendo."
+    },
+    {
+        "autor": "Ayn Rand",
+        "frase": "Um homem criativo é motivado pelo desejo de alcançar, não pelo desejo de vencer os outros."
+    },
+    {
+        "autor": "Platão",
+        "frase": "Tente mover o mundo – o primeiro passo será mover a si mesmo."
+    },
+    {
+        "autor": "Jufras Menhal",
+        "frase": "Trate bem as oportunidades. Elas são reservadas e não aparecem pra qualquer um; algumas se ofendem fácil e podem nunca mais voltar."
+    },
+    {
+        "autor": "Albert Einstein",
+        "frase": "A mente que se abre a uma nova ideia jamais voltará ao seu tamanho original."
+    },
+    {
+        "autor": "Henry Ford",
+        "frase": "O insucesso é uma oportunidade para recomeçar com mais inteligência."
+    },
+    {
+        "autor": "Roger Crawford",
+        "frase": "Ter problemas na vida é inevitável, ser derrotado por eles é opcional."
+    },
+    {
+        "autor": "Nelson Mandela",
+        "frase": "Tudo é considerado impossível até acontecer."
+    },
+    {
+        "autor": "Rui Barbosa",
+        "frase": "A diferença entre o inteligente e o sábio, é que o sábio pensa a longo prazo."
+    },
+    {
+        "autor": "Epicuro",
+        "frase": "A verdadeira riqueza não consiste em ter grandes posses, mas em ter poucas necessidades."
+    },
+    {
+        "autor": "Benjamin Franklin",
+        "frase": "Cuidado com as pequenas despesas. Um pequeno vazamento afundará um grande navio."
+    },
+    {
+        "autor": "Warren Buffet",
+        "frase": "Risco vem de você não saber o que está fazendo."
+    },
+    {
+        "autor": "Thomas Jefferson",
+        "frase": "Jamais gaste seu dinheiro antes de você possuí-lo."
+    },
+    {
+        "autor": "Lao Tzu",
+        "frase": "Uma jornada de mil quilômetros precisa começar com um simples passo."
+    }
+]
